e1cd8ec7e8277f5eeb3ab156006219a3649ff7d4
Viktor Köves <3187531+vkoves@users.noreply.github.com>
2025-04-17 13:08:51 -0500

    Fix Claude init

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Copy Claude files to root
-cp -r .dotfiles/claude ~/
+# Copy .claude directory to root (so it's at `~/.claude/CLAUDE.md`)
+# @see https://docs.anthropic.com/en/docs/agents-and-tools/claude-code/overview#determine-memory-type
+cp -r .dotfiles/.claude ~/
 
 mv .dotfiles/.zshrc ~/
